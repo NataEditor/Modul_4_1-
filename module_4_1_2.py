053e11e3a1824f45ae5ec893d936233638d7267b
NataEditor <nako_al@mail.ru>
2024-08-19 14:35:13 +0300

    Задача "А как делить?":
    Задача "А как делить?":
    В школе нам говорили, что на 0 делить нельзя. Высшая же математика опровергает это и говорит, что результат при делении на 0 будет стремиться к бесконечности.
    Давайте реализуем оба способа, чтобы у вас всегда был выбор!
    Создайте модули fake_math и true_math в которых создайте функции отвечающие за деление, но разными способами.

---
+++ module_4_1_2.py
@@ -0,0 +1,11 @@
+import fake_math as div_sh
+import true_math as div_Hm
+
+result1 = div_sh.divide(69,3)
+result2 = div_sh.divide(3,0)
+result3 = div_Hm.divide(49,7)
+result4 = div_Hm.divide(15,0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
